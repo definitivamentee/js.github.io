0c686324532b328d924ef8e05da8274b19d07973
definitivamentee <166766852+definitivamentee@users.noreply.github.com>
2024-05-18 20:17:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var nombre="Elisa";
+var apellido="Gutierrez";
+alert("Bienvenida " + nombre + " " + apellido)
+console.log("Bienvenida " + nombre +" " + apellido)
+
+var numero=8;
+var numero2=5;
+var suma=numero+numero2
+console.log("El resultado de la suma es"+" "+ suma)
+
+var numero3= suma+10
+console.log("El resultado final es" +" "+ numero3 )
